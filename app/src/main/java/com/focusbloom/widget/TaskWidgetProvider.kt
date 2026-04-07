11d39977a63ac761ee3765ec06e1db234dfec026
ygs <ygs@example.com>
2026-04-07 14:43:26 +0800

    fix: manifest component paths resolved incorrectly causing ClassNotFoundException, remove unused Glance widgets to simplify

--- app/src/main/java/com/focusbloom/widget/TaskWidgetProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.focusbloom.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TaskWidgetProvider : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TaskWidget()
-}
--- app/src/main/java/com/focusbloom/widget/TimerWidgetProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.focusbloom.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class TimerWidgetProvider : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = TimerWidget()
-}
